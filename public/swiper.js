db59b51b3bf96bb858e08c2b3ba17da45ed8b4d4
unknown <norikendall@gmail.com>
2024-07-25 16:42:52 +0300

    another commit

--- public/swiper.js
+++ public/swiper.js
@@ -1,21 +1,27 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
+
+
+var swiper = new Swiper('.swiper-container', {
   loop: true,
-
-  // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+      el: '.swiper-pagination',
+      clickable: true,
   },
-
-  // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
   },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+      // when window width is >= 640px
+      640: {
+          slidesPerView: 2,
+          spaceBetween: 20
+      },
+      // when window width is >= 768px
+      768: {
+          slidesPerView: 3,
+          spaceBetween: 30
+      }
+  }
 });
