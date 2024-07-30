cea10591a764b152c4c0be2ba15ac995e450288f
unknown <norikendall@gmail.com>
2024-07-30 12:51:15 +0300

    another commit

---
+++ public/main.js
@@ -0,0 +1,38 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    slidesPerView: 3,
+  spaceBetween: 20,   
+    direction: 'horizontal',    
+    loop: true,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },    
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,   
+      
+    },    
+   
+  breakpoints: {
+      // when window width is >= 640px
+      320: {
+          slidesPerView: 1,
+        
+      },
+      
+      960: {
+          slidesPerView: 2,
+    
+      },
+      1200: {
+        slidesPerView: 3,
+    
+    }
+  }
+  
+    
+  });
+  
