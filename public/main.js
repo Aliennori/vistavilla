b0f8031d42fbb5169ad54e659d9d0d0ec059b170
unknown <norikendall@gmail.com>
2024-07-30 17:46:18 +0300

    another commit

--- public/main.js
+++ public/main.js
@@ -1,3 +1,4 @@
+//mobile navbar
 const hamburger= document.querySelector('#hamburger');
 const navlink= document.querySelector('#navlink');
 
@@ -6,7 +7,7 @@ hamburger.addEventListener('click', () => {
    
 });
 
-
+//swiper slides
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     slidesPerView: 3,
@@ -44,4 +45,49 @@ const swiper = new Swiper('.swiper', {
   
     
   });
+
+
+
+  //scrollreveal
+  const sr= ScrollReveal ({
+    origin: 'bottom',
+    distance: '60px',
+    duration: 3000,
+    delay: 600,
+
+  });
   
+  sr.reveal('.heroText', {origin: 'top'});
+  sr.reveal('.card', {distance: '100px', interval: 100});
+  sr.reveal('.left', {origin: 'left'});
+  sr.reveal('.right', {origin: 'right', delay: 800});
+  sr.reveal('.testimonial', {delay: 800});
+  sr.reveal('.testimonial-title');
+  sr.reveal('.testimonial-slider', {delay: 1000});
+  sr.reveal('.prop-title');
+  sr.reveal('.prop-text', {delay: 800});
+  sr.reveal('.prop-grid', {delay: 1000});
+  sr.reveal('.stats');
+  sr.reveal('.stat-item', {distance: '100px', interval: 100});
+  sr.reveal('.article-title' );
+  sr.reveal('.article-text', {delay:800});
+
+  sr.reveal('.news-flex', {
+    distance: '100px',
+    interval: 100,
+    delay: 1000,
+
+  });
+  sr.reveal('.action');
+  sr.reveal('.action-title', {delay:800});
+  sr.reveal('.action-text', {delay:800});
+  sr.reveal('.action-btn', {delay:800});
+
+  sr.reveal('.footer-item', {
+    distance: '100px',
+    interval: 100,
+    delay: 1000,
+  });
+  sr.reveal('.copyright');
+
+
