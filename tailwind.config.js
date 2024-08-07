a94d329d84f5aa2d8a74c98448606508fab20520
unknown <norikendall@gmail.com>
2024-08-07 23:42:40 +0300

    another commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
           two: 'rgb(0,158,206)',
           three: '#f8f9fa',
           four: 'rgb(250,250,250)',
-
+          
         }        
     },
     backgroundImage: {
